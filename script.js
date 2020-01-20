9fcc5a6e569435c8d4c7327d8037e3a27a5df414
doskzorak <tgmartin1985@live.com>
2020-01-19 20:00:52 -0500

    Updatedgame

---
+++ script.js
@@ -0,0 +1,44 @@
+function makeColourValue() {
+    return Math.round(Math.random()*255);
+}
+
+function setButtonColour(button, red, green, blue) {
+    button.setAttribute('style',
+                        'background-color: rgb('+ red +','+ green +','+ blue +');'
+                        );
+}
+
+var buttons = document.getElementsByClassName('colourButton');
+var answerMessage = document.getElementById('answer');
+var heading = document.getElementById('colourValue');
+
+function startGame() {
+
+    answerMessage.innerHTML = "";
+
+    var answerButton = Math.round(Math.random() * (buttons.length - 1));
+
+    for (var i = 0; i < buttons.length; i++) {
+
+        var red = makeColourValue();
+        var green = makeColourValue();
+        var blue = makeColourValue();
+
+        setButtonColour(buttons[i], red, blue, green);
+
+        if (i === answerButton) {
+            heading.innerHTML = `(${red}, ${green}, ${blue})`;
+        }
+
+        buttons[i].addEventListener('click', function() {
+            if (this === buttons[answerButton]) {
+                answerMessage.innerHTML = `Correct!`;
+            } else {
+                answerMessage.innerHTML = `Wrong answer! Guess again!`;
+            }
+        });
+    }
+}
+
+startGame();
+document.getElementById('resetButton').addEventListener('click', startGame);
